bce832d7b427a9bbcc1f7eba02031104a8f0ac7b
evendalen <even.dalen@live.no>
2018-04-04 14:46:48 +0200

    Changed complier to amd

--- dist/index.js
+++ dist/index.js
@@ -16,20 +16,20 @@ var __assign = (this && this.__assign) || Object.assign || function(t) {
     }
     return t;
 };
-System.register("utils", [], function (exports_1, context_1) {
+define("utils", ["require", "exports"], function (require, exports) {
     "use strict";
-    var __moduleName = context_1 && context_1.id;
+    Object.defineProperty(exports, "__esModule", { value: true });
     function calculateTouchPosistion(rectangle, pageOffsetX, pageOffsetY) {
         return {
             x: pageOffsetX - rectangle.left,
             y: pageOffsetY - rectangle.top
         };
     }
-    exports_1("calculateTouchPosistion", calculateTouchPosistion);
+    exports.calculateTouchPosistion = calculateTouchPosistion;
     function distance(fromPoint, toPoint) {
         return Math.sqrt(Math.pow(fromPoint.x - toPoint.x, 2) + Math.pow(fromPoint.y - toPoint.y, 2));
     }
-    exports_1("distance", distance);
+    exports.distance = distance;
     function removeOverlapping(buffer, compareCoords, radius) {
         var newBuffer = [];
         buffer.forEach(function (trace) {
@@ -48,178 +48,160 @@ System.register("utils", [], function (exports_1, context_1) {
         });
         return newBuffer;
     }
-    exports_1("removeOverlapping", removeOverlapping);
-    return {
-        setters: [],
-        execute: function () {
-        }
-    };
+    exports.removeOverlapping = removeOverlapping;
 });
-System.register("index", ["utils", "../node_modules/eventemitter3/index"], function (exports_2, context_2) {
+define("index", ["require", "exports", "utils", "../node_modules/eventemitter3/index"], function (require, exports, utils, EventEmitter) {
     "use strict";
-    var __moduleName = context_2 && context_2.id;
-    var utils, EventEmitter, SymbolCanvas, CanvasController, thisWindow;
-    return {
-        setters: [
-            function (utils_1) {
-                utils = utils_1;
-            },
-            function (EventEmitter_1) {
-                EventEmitter = EventEmitter_1;
-            }
-        ],
-        execute: function () {
-            SymbolCanvas = (function (_super) {
-                __extends(SymbolCanvas, _super);
-                function SymbolCanvas(element) {
-                    var _this = _super.call(this) || this;
-                    _this.previousCoords = null;
-                    _this.isPressingDown = false;
-                    _this.drewBeforeRelease = false;
-                    _this.onMouseDown = function (event) {
-                        event.preventDefault();
-                        _this.isPressingDown = true;
-                    };
-                    _this.onTouchStart = function (event) {
-                        event.preventDefault();
-                        _this.isPressingDown = true;
-                    };
-                    _this.onMouseUp = function (event) {
-                        _this.onPressRelease(event);
-                    };
-                    _this.onTouchEnd = function (event) {
-                        _this.onPressRelease(event);
-                    };
-                    _this.onPressRelease = function (event) {
-                        event.preventDefault();
-                        _this.isPressingDown = false;
-                        _this.previousCoords = null;
-                        if (!_this.drewBeforeRelease) {
-                            return;
-                        }
-                        _this.drewBeforeRelease = false;
-                        _this.emit('release');
-                    };
-                    _this.normalizeTouchEventCoords = function (event) {
-                        var target = event.target;
-                        var rect = target.getBoundingClientRect();
-                        var targetTouch = event.targetTouches[0];
-                        var offsetX = targetTouch.pageX;
-                        var offsetY = targetTouch.pageY;
-                        return utils.calculateTouchPosistion(rect, offsetX, offsetY);
-                    };
-                    _this.onTouchMove = function (event) {
-                        if (!_this.isPressingDown) {
-                            return;
-                        }
-                        if (!_this.drewBeforeRelease) {
-                            _this.drewBeforeRelease = true;
-                        }
-                        _this.handleNewCoordinates(_this.normalizeTouchEventCoords(event));
-                    };
-                    _this.onMouseMove = function (event) {
-                        if (!_this.isPressingDown) {
-                            return;
-                        }
-                        event.preventDefault();
-                        if (!_this.drewBeforeRelease) {
-                            _this.drewBeforeRelease = true;
-                        }
-                        _this.handleNewCoordinates({
-                            x: event.offsetX,
-                            y: event.offsetY
-                        });
-                    };
-                    _this.handleNewCoordinates = function (coords) {
-                        var previousCoords = _this.previousCoords === null ? _this.previousCoords : __assign({}, _this.previousCoords);
-                        _this.emit('draw', __assign({}, coords), previousCoords);
-                        _this.previousCoords = coords;
-                    };
-                    _this.drawLine = function (fromCoords, toCoords, color, lineWidth) {
-                        if (color === void 0) { color = "#A0A3A6"; }
-                        if (lineWidth === void 0) { lineWidth = 5; }
-                        _this.context.lineWidth = lineWidth;
-                        _this.context.strokeStyle = color;
-                        _this.context.beginPath();
-                        _this.context.moveTo(fromCoords.x, fromCoords.y);
-                        _this.context.lineTo(toCoords.x, toCoords.y);
-                        _this.context.closePath();
-                        _this.context.stroke();
-                    };
-                    _this.drawCircle = function (coords, radius, fillStyle) {
-                        if (radius === void 0) { radius = 10; }
-                        if (fillStyle === void 0) { fillStyle = "white"; }
-                        _this.context.beginPath();
-                        _this.context.arc(coords.x, coords.y, radius, 0, 2 * Math.PI, false);
-                        _this.context.fillStyle = 'white';
-                        _this.context.fill();
-                    };
-                    _this.element = element;
-                    _this.context = element.getContext('2d');
-                    _this.element.addEventListener('mousedown', _this.onMouseDown);
-                    _this.element.addEventListener('touchstart', _this.onTouchStart);
-                    _this.element.addEventListener('mouseup', _this.onMouseUp);
-                    _this.element.addEventListener('touchend', _this.onTouchEnd);
-                    _this.element.addEventListener('mousemove', _this.onMouseMove);
-                    _this.element.addEventListener('touchmove', _this.onTouchMove);
-                    return _this;
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var SymbolCanvas = (function (_super) {
+        __extends(SymbolCanvas, _super);
+        function SymbolCanvas(element) {
+            var _this = _super.call(this) || this;
+            _this.previousCoords = null;
+            _this.isPressingDown = false;
+            _this.drewBeforeRelease = false;
+            _this.onMouseDown = function (event) {
+                event.preventDefault();
+                _this.isPressingDown = true;
+            };
+            _this.onTouchStart = function (event) {
+                event.preventDefault();
+                _this.isPressingDown = true;
+            };
+            _this.onMouseUp = function (event) {
+                _this.onPressRelease(event);
+            };
+            _this.onTouchEnd = function (event) {
+                _this.onPressRelease(event);
+            };
+            _this.onPressRelease = function (event) {
+                event.preventDefault();
+                _this.isPressingDown = false;
+                _this.previousCoords = null;
+                if (!_this.drewBeforeRelease) {
+                    return;
                 }
-                return SymbolCanvas;
-            }(EventEmitter));
-            exports_2("SymbolCanvas", SymbolCanvas);
-            CanvasController = (function (_super) {
-                __extends(CanvasController, _super);
-                function CanvasController(canvas, options) {
-                    if (options === void 0) { options = { isErasing: false, eraseRadius: 10 }; }
-                    var _this = _super.call(this) || this;
-                    _this.traceIndex = 0;
-                    _this.buffer = [[]];
-                    _this.handleDraw = function (currentCoords, previousCoords) {
-                        if (_this.options.isErasing) {
-                            _this.buffer = utils.removeOverlapping(_this.buffer, currentCoords, _this.options.eraseRadius);
-                            _this.canvas.drawCircle(currentCoords, _this.options.eraseRadius);
-                        }
-                        else {
-                            _this.buffer[_this.traceIndex].push(currentCoords);
-                            if (previousCoords !== null) {
-                                _this.canvas.drawLine(previousCoords, currentCoords);
-                            }
-                        }
-                    };
-                    _this.handleRelease = function () {
-                        _this.buffer.push([]);
-                        _this.traceIndex += 1;
-                        _this.emit('release', __assign({}, _this.buffer));
-                    };
-                    _this.markTraceGroups = function (traceGroupIndexes, color) {
-                        if (color === void 0) { color = 'red'; }
-                        traceGroupIndexes.forEach(function (groupIndex) {
-                            var trace = _this.buffer[groupIndex];
-                            if (trace === undefined) {
-                                throw new Error("Attempted to mark trace not in buffer. groupIndex: " + groupIndex + ". buffer: " + JSON.stringify(_this.buffer));
-                            }
-                            for (var i = 0; i < trace.length - 2; i++) {
-                                _this.canvas.drawLine(trace[i], trace[i + 1], color, 4);
-                            }
-                        });
-                    };
-                    _this.canvas = canvas;
-                    canvas.on('draw', _this.handleDraw);
-                    canvas.on('release', _this.handleRelease);
-                    _this.options = options;
-                    _this.traceIndex = 0;
-                    return _this;
+                _this.drewBeforeRelease = false;
+                _this.emit('release');
+            };
+            _this.normalizeTouchEventCoords = function (event) {
+                var target = event.target;
+                var rect = target.getBoundingClientRect();
+                var targetTouch = event.targetTouches[0];
+                var offsetX = targetTouch.pageX;
+                var offsetY = targetTouch.pageY;
+                return utils.calculateTouchPosistion(rect, offsetX, offsetY);
+            };
+            _this.onTouchMove = function (event) {
+                if (!_this.isPressingDown) {
+                    return;
                 }
-                return CanvasController;
-            }(EventEmitter));
-            exports_2("CanvasController", CanvasController);
-            console.log("Im running here", window);
-            thisWindow = window;
-            if (typeof window !== 'undefined') {
-                thisWindow.SymbolCanvas = SymbolCanvas;
-                thisWindow.CanvasController = CanvasController;
-            }
+                if (!_this.drewBeforeRelease) {
+                    _this.drewBeforeRelease = true;
+                }
+                _this.handleNewCoordinates(_this.normalizeTouchEventCoords(event));
+            };
+            _this.onMouseMove = function (event) {
+                if (!_this.isPressingDown) {
+                    return;
+                }
+                event.preventDefault();
+                if (!_this.drewBeforeRelease) {
+                    _this.drewBeforeRelease = true;
+                }
+                _this.handleNewCoordinates({
+                    x: event.offsetX,
+                    y: event.offsetY
+                });
+            };
+            _this.handleNewCoordinates = function (coords) {
+                var previousCoords = _this.previousCoords === null ? _this.previousCoords : __assign({}, _this.previousCoords);
+                _this.emit('draw', __assign({}, coords), previousCoords);
+                _this.previousCoords = coords;
+            };
+            _this.drawLine = function (fromCoords, toCoords, color, lineWidth) {
+                if (color === void 0) { color = "#A0A3A6"; }
+                if (lineWidth === void 0) { lineWidth = 5; }
+                _this.context.lineWidth = lineWidth;
+                _this.context.strokeStyle = color;
+                _this.context.beginPath();
+                _this.context.moveTo(fromCoords.x, fromCoords.y);
+                _this.context.lineTo(toCoords.x, toCoords.y);
+                _this.context.closePath();
+                _this.context.stroke();
+            };
+            _this.drawCircle = function (coords, radius, fillStyle) {
+                if (radius === void 0) { radius = 10; }
+                if (fillStyle === void 0) { fillStyle = "white"; }
+                _this.context.beginPath();
+                _this.context.arc(coords.x, coords.y, radius, 0, 2 * Math.PI, false);
+                _this.context.fillStyle = 'white';
+                _this.context.fill();
+            };
+            _this.element = element;
+            _this.context = element.getContext('2d');
+            _this.element.addEventListener('mousedown', _this.onMouseDown);
+            _this.element.addEventListener('touchstart', _this.onTouchStart);
+            _this.element.addEventListener('mouseup', _this.onMouseUp);
+            _this.element.addEventListener('touchend', _this.onTouchEnd);
+            _this.element.addEventListener('mousemove', _this.onMouseMove);
+            _this.element.addEventListener('touchmove', _this.onTouchMove);
+            return _this;
         }
-    };
+        return SymbolCanvas;
+    }(EventEmitter));
+    exports.SymbolCanvas = SymbolCanvas;
+    var CanvasController = (function (_super) {
+        __extends(CanvasController, _super);
+        function CanvasController(canvas, options) {
+            if (options === void 0) { options = { isErasing: false, eraseRadius: 10 }; }
+            var _this = _super.call(this) || this;
+            _this.traceIndex = 0;
+            _this.buffer = [[]];
+            _this.handleDraw = function (currentCoords, previousCoords) {
+                if (_this.options.isErasing) {
+                    _this.buffer = utils.removeOverlapping(_this.buffer, currentCoords, _this.options.eraseRadius);
+                    _this.canvas.drawCircle(currentCoords, _this.options.eraseRadius);
+                }
+                else {
+                    _this.buffer[_this.traceIndex].push(currentCoords);
+                    if (previousCoords !== null) {
+                        _this.canvas.drawLine(previousCoords, currentCoords);
+                    }
+                }
+            };
+            _this.handleRelease = function () {
+                _this.buffer.push([]);
+                _this.traceIndex += 1;
+                _this.emit('release', __assign({}, _this.buffer));
+            };
+            _this.markTraceGroups = function (traceGroupIndexes, color) {
+                if (color === void 0) { color = 'red'; }
+                traceGroupIndexes.forEach(function (groupIndex) {
+                    var trace = _this.buffer[groupIndex];
+                    if (trace === undefined) {
+                        throw new Error("Attempted to mark trace not in buffer. groupIndex: " + groupIndex + ". buffer: " + JSON.stringify(_this.buffer));
+                    }
+                    for (var i = 0; i < trace.length - 2; i++) {
+                        _this.canvas.drawLine(trace[i], trace[i + 1], color, 4);
+                    }
+                });
+            };
+            _this.canvas = canvas;
+            canvas.on('draw', _this.handleDraw);
+            canvas.on('release', _this.handleRelease);
+            _this.options = options;
+            _this.traceIndex = 0;
+            return _this;
+        }
+        return CanvasController;
+    }(EventEmitter));
+    exports.CanvasController = CanvasController;
+    console.log("Im running here", window);
+    var thisWindow = window;
+    if (typeof window !== 'undefined') {
+        thisWindow.SymbolCanvas = SymbolCanvas;
+        thisWindow.CanvasController = CanvasController;
+    }
 });
 //# sourceMappingURL=index.js.map
